ca866ee20366c6da4857da256436f66cd3a77e10
Lorenzo Bataloni <101251888+bata26Move@users.noreply.github.com>
2022-11-12 15:12:05 +0100

    Move controller to application

--- backend/application.py
+++ backend/application.py
@@ -11,6 +11,33 @@ from markupsafe import escape
 load_dotenv()
 application = Flask(__name__)
 
+@application.route('/accomodations/<accomodation_id>' , methods = ['DELETE'])
+def deleteAccomodationById (accomodation_id):
+    accomodationId = escape(accomodation_id)
+    user = {
+        "type" : "admin"
+    }
+    result = AccomodationsManager.deleteAccomodation(accomodationId , user)
+    print(f"delete result : {result}")
+    return "" , 200
+
+@application.route('/accomodations/<accomodation_id>' , methods = ['GET'])
+def getAccomodationById (accomodation_id):
+    accomodationId = escape(accomodation_id)
+    result = AccomodationsManager.getAccomodationsFromId(accomodationId)
+    return result , 200
+
+@application.route('/accomodations' , methods = ['GET'])
+def getAccomodations():
+    args = request.args
+    city = args.get("city")
+    guests = args.get("guests")
+    start_date = args.get("start_date")
+    end_date = args.get("end_date")
+    result = AccomodationsManager.getFilteredAccomodation(start_date  , end_date  , city , guests)
+    print(f"result : {result}")
+    result["_id"] = str(result["_id"])
+    return result , 200
 
 if __name__ == "__main__":
     application.run(threaded=True , debug=True , use_reloader=False)
