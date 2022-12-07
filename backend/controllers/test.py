7ad35c7ae646d18e0ff199b20cf09dac07379ae0
Lorenzo Bataloni <101251888+bata26Move@users.noreply.github.com>
2022-12-07 12:24:25 +0100

    Add presentation

--- backend/controllers/test.py
+++
@@ -1,6 +0,0 @@
-import bcrypt
-
-pwd = "AdminAdmin"
-hash = "$2b$12$EJN3ChUbxNSjX07f1RCxb.C1ArPJ/bICl0kPzyY4R4.wtzYmn.ZyK"
-
-print(bcrypt.checkpw(pwd.encode("utf-8") , hash.encode("utf-8") ))
--- backend/test.py
+++ backend/test.py
@@ -1,8 +1,60 @@
-import bcrypt
+#import bcrypt
+#
+#clienthash = "$2b$12$S0JsqH4t87pLBM1AWTXeTOPMh2/fSskcygTF171Ro8MgdOtyLpbEu"
+#pwd = "AdminAdmin"
+#salt = bcrypt.gensalt(12)
+#dbHash = bcrypt.hashpw(pwd.encode('utf-8'), salt).decode('utf-8')
+#print(dbHash)
+#print(bcrypt.checkpw(pwd.encode('utf-8'), dbHash.encode('utf8')))
+from controllers.connection import MongoManager
+from random import randint
+import pymongo
+from bson.objectid import ObjectId
 
-clienthash = "$2b$12$S0JsqH4t87pLBM1AWTXeTOPMh2/fSskcygTF171Ro8MgdOtyLpbEu"
-pwd = "AdminAdmin"
-salt = bcrypt.gensalt(12)
-dbHash = bcrypt.hashpw(pwd.encode('utf-8'), salt).decode('utf-8')
-print(dbHash)
-print(bcrypt.checkpw(pwd.encode('utf-8'), dbHash.encode('utf8')))
+users = [
+{
+	"_id" : "637ce1a04ed62608566c5fa7",
+	"name" : "Luca"
+},
+{
+	"_id" : "637ce1a04ed62608566c5fa8",
+	"name" : "Lucia"
+},
+{
+	"_id" : "637ce1a04ed62608566c5fa9",
+	"name" : "Anna"
+},
+{
+	"_id" : "637ce1a04ed62608566c5faa",
+	"name" : "Giovanni"
+},
+{
+	"_id" : "637ce1a04ed62608566c5fab",
+	"name" : "Valeria"
+},
+{
+	"_id" : "637ce1a04ed62608566c5fac",
+	"name" : "Angela"
+},
+{
+	"_id" : "637ce1a04ed62608566c5fad",
+	"name" : "Rosa"
+},
+{
+	"_id" : "637ce1a04ed62608566c5fae",
+	"name" : "Valentina"
+}]
+
+client = MongoManager.getInstance()
+db = client["myvacation"]
+accomodationCollection = db["activities"]
+cursor = list(accomodationCollection.find())
+
+counter = 0
+for accomodation in cursor:
+    user = users[randint(0 , len(users)- 1)]
+    try:
+        accomodationCollection.update_one({"_id" : accomodation["_id"]} , {"$set" : {"host_id" : ObjectId(user["_id"]) , "host_name" : user["name"]}})
+        print(f"Inserito correttamente: {counter}")
+    except Exception as e:
+        print(f"Impossibile aggiornare : {e}" )
