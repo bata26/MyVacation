c1363777ceaa0bb00d8197c11d687fc4e2589574
Lorenzo Bataloni <101251888+bata26Move@users.noreply.github.com>
2022-12-10 15:28:38 +0100

    Fix auth

--- frontend/myvacation/src/api/api.js
+++ frontend/myvacation/src/api/api.js
@@ -2,17 +2,17 @@ import axios from "axios";
 import useAuth from "../hooks/useAuth";
 
 const BASE_URL = "http://127.0.0.1:5000";
-const {userID , role}  = useAuth();
-const authorizationObject = {
-    "userID" : userID,
-    "role" : role
-};
+//const {userID , role}  = useAuth();
+//const authorizationObject = {
+//    "userID" : userID,
+//    "role" : role
+//};
 const instance = axios.create({
     baseURL: BASE_URL,
     headers: {
         "Access-Control-Allow-Origin" :"*",
         "crossDomain" : true,
-        "Authorization" : JSON.stringify(authorizationObject)
+        //"Authorization" : localStorage.getItem
     }
 
 });
