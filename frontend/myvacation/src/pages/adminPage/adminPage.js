cbdb353556dbb0b3d1e55e986874cd8531805868
EdoPan <81682326+EdoPan@users.noreply.github.com>
2022-12-27 15:42:55 +0100

    Add admin analytics

--- frontend/myvacation/src/pages/adminPage/adminPage.js
+++ frontend/myvacation/src/pages/adminPage/adminPage.js
@@ -42,13 +42,138 @@ const AdminPage = () => {
                     </Container>
                 </Box>
 
-                {/* Tabella città - media prezzi */}
 
-                {/* Tabella top 10 host */}
 
-                {/* Card totale annunci */}
+                <Container maxWidth="sm">
+                    <Typography
+                        component="h2"
+                        variant="h4"
+                        align="center"
+                        color="text.primary"
+                        gutterBottom
+                    >
+                        Analytics
+                    </Typography>
+                </Container>
 
-                {/* Andamento iscrizioni */}
+
+
+                <Container maxWidth="md" sx={{ mt: 4 }}>
+
+                    {/* Grafico andamento iscrizioni */}
+                    <Card sx={{ mb: 4 }}>
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div" align='center'>
+                                This month subscribed
+                            </Typography>
+                            <Typography gutterBottom variant="h5" component="div" align='center'>
+                                10 users
+                            </Typography>
+                        </CardContent>
+                    </Card>
+
+                    <Grid container columnSpacing={1.4}>
+
+                        {/* Tabella città - media prezzi */}
+                        <Grid item xs={4} sm={2.7}>
+                            <TableContainer component={Paper} style={{ marginBottom: 50 + 'px' }} >
+                                <Table size="small">
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell align="center" style={{ fontWeight: 'bold' }}>Average price per city</TableCell>
+
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+
+                                        <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                                            <TableCell align="center">Roma </TableCell>
+                                            <TableCell align="center">350€</TableCell>
+
+                                        </TableRow>
+
+                                        <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                                            <TableCell align="center">Milano</TableCell>
+                                            <TableCell align="center">650€</TableCell>
+
+                                        </TableRow>
+
+                                        <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                                            <TableCell align="center">Torino</TableCell>
+                                            <TableCell align="center">450€</TableCell>
+
+                                        </TableRow>
+
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                        </Grid>
+
+                        <Grid item xs={4} sm={2.7}>
+                            {/* Tabella top 10 host */}
+                            <TableContainer component={Paper} style={{ marginBottom: 50 + 'px' }} >
+                                <Table size="small">
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell align="center" style={{ fontWeight: 'bold' }}>Top 10 hosts</TableCell>
+
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+
+                                        <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                                            <TableCell align="center">Luca </TableCell>
+                                            <TableCell align="center">Rossi</TableCell>
+
+                                        </TableRow>
+
+                                        <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                                            <TableCell align="center">Matteo</TableCell>
+                                            <TableCell align="center">Verdi</TableCell>
+
+                                        </TableRow>
+
+                                        <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                                            <TableCell align="center">Mario</TableCell>
+                                            <TableCell align="center">Grossi</TableCell>
+
+                                        </TableRow>
+
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+
+
+                        </Grid>
+
+                        {/* Card totale annunci */}
+                        <Grid item xs={4} sm={3.2}>
+                            <Card>
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div" align='center'>
+                                        Number of advertisements
+                                    </Typography>
+                                    <Typography gutterBottom variant="h5" component="div" align='center'>
+                                        500
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                        {/* Card totale utenti */}
+                        <Grid item xs={4} sm={3.2}>
+                            <Card>
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div" align='center'>
+                                        Number of users
+                                    </Typography>
+                                    <Typography gutterBottom variant="h5" component="div" align='center'>
+                                        100
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    </Grid>
+                </Container>
 
 
                 <Container maxWidth="md" sx={{ mt: 4 }}>
